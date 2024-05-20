f3f359c9402a4cee40f56494c7065884230b9845
dionizio1903 <59143999+dionizio1903@users.noreply.github.com>
2024-05-20 18:58:42 -0400

    Add files via upload

---
+++ front_end/java_script/sintaxe/script.js
@@ -0,0 +1,18 @@
+/*alert("hello word")*/
+
+console.log("intrução1")
+
+var nome ="Jose"
+
+console.log  (nome)
+
+var a = 6;
+if(a>6){
+    document.write("maior que 6")
+}else if (a==6){
+    document.write("igual 6")
+}
+else {
+    document.write("menor que 6")
+}
+console.log(a)
