def74316fb477acafd1c44ccc49182087102506a
dionizio1903 <59143999+dionizio1903@users.noreply.github.com>
2024-05-24 18:52:02 -0400

    Add files via upload

---
+++ front_end/Carrosel/script/script.js
@@ -0,0 +1,15 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    proximaImagem();
+}, 3000)
+
+function proximaImagem(){
+    count ++ ;
+    if (count > 4) {
+        count = 1;
+    }
+    
+    document.getElementById("radio" + count).checked = true;
+}
